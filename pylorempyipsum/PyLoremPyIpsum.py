0b1882784beef55a22d6d3501ddc4fc59e60f537
Vladislav Bezverkhiy <rabbiabram@gmail.com>
2014-01-05 23:29:04 +0600

    Added get_paragraph and get_text methods with tests

--- pylorempyipsum/PyLoremPyIpsum.py
+++ pylorempyipsum/PyLoremPyIpsum.py
@@ -16,24 +16,63 @@ class PyLoremPyIpsum(object):
             yield self.words_list[i]
 
 
-    def get_string(self,max_words_in_string=10):
-        string_length = 0
-        if max_words_in_string <= 0:
+    def get_string(self,strings_count, max_words_in_string=10):
+        if max_words_in_string <= 0 or strings_count <= 0:
             yield ''
         else:
+            for j in range(strings_count):
+                if max_words_in_string == 1:
+                    string_length = 1
+                else:
+                    string_length = random.randint(1, max_words_in_string)
+                words = 0
+                string = ''
+                for word in self.get_word(string_length):
+                    string += word
+                    words += 1
+                    if words > 0 and words < string_length:
+                        string += ' '
+                    elif words == string_length:
+                        string += '.'
+                yield string
 
-            if max_words_in_string == 1:
-                string_length = 1
+    def get_paragraphs(self, paragraphs_count, max_strings_in_paragraph=10, max_string_length=10):
+        if max_strings_in_paragraph <= 0 or paragraphs_count <= 0:
+            yield ''
+        else:
+            for j in range(paragraphs_count):
+                if max_strings_in_paragraph == 1:
+                    paragraph_length = max_strings_in_paragraph
+                else:
+                    paragraph_length = random.randint(1, max_strings_in_paragraph)
+                paragraph = ''
+                strings = 0
+
+                for string in self.get_string(paragraph_length, max_string_length):
+                    paragraph += string
+                    strings += 1
+                    if strings > 1 and  strings < paragraph_length:
+                        paragraph += ' '
+                yield paragraph
+
+    def get_text(self, max_chars=500):
+        i = 0
+        text = ''
+        failed = 0
+        if (max_chars <= 0):
+            return ''
+        while i <= max_chars and failed < 3:
+            sub_text = ''
+            for t in self.get_paragraphs(1, random.randint(1, 10), random.randint(1, 15)):
+                sub_text = t
+            if len(text) + len(sub_text) + 1 <= max_chars:
+                text += sub_text + '\n'
+                i = len(text)
             else:
-                string_length = random.randint(1, max_words_in_string)
-            words = 0
-            string = ''
-            for word in self.get_word(string_length):
-                string += word
-                words += 1
-                if words > 0 and words < string_length:
-                    string += ' '
-                elif words == string_length:
-                    string += '.'
-            yield string
+                failed += 1
+
+        return text
+
+
+
 
--- pylorempyipsum/tests.py
+++ pylorempyipsum/tests.py
@@ -1,5 +1,5 @@
 __author__ = 'rabbiabram@gmail.com'
-import unittest
+import unittest, random
 from pylorempyipsum import PyLoremPyIpsum
 
 class TestPyLoremPyIpsum(unittest.TestCase):
@@ -17,7 +17,11 @@ class TestPyLoremPyIpsum(unittest.TestCase):
         for i in self.lorem_object.get_word(1):
             new_word = i
         self.assertIsNotNone(new_word)
-
+        words_count = random.randint(0, 255)
+        words = []
+        for i in self.lorem_object.get_word(words_count):
+            words.append(i)
+        self.assertEqual(words_count, len(words))
 
     def testGetString(self):
         string = None
@@ -28,3 +32,37 @@ class TestPyLoremPyIpsum(unittest.TestCase):
         for i in self.lorem_object.get_string(1):
             new_string = i
         self.assertNotEqual('', new_string)
+        string_count = random.randint(0, 255)
+        strings = []
+        for i in self.lorem_object.get_string(string_count):
+            strings.append(i)
+        self.assertEqual(string_count, len(strings))
+
+    def testGetParagraph(self):
+        paragraph = None
+        for i in self.lorem_object.get_paragraphs(0):
+            paragraph = i
+        self.assertEqual('', paragraph)
+
+        new_paragraph = ''
+        for p in self.lorem_object.get_paragraphs(1):
+            new_paragraph = p
+        self.assertNotEqual('', new_paragraph)
+        paragraphs_count = random.randint(0, 10)
+        paragraphs = []
+        for p in self.lorem_object.get_paragraphs(paragraphs_count):
+            paragraphs.append(p)
+        self.assertEqual(paragraphs_count, len(paragraphs))
+
+    def testGetText(self):
+        empty_text = self.lorem_object.get_text(0)
+        self.assertEqual(0, len(empty_text))
+
+        typical_text = self.lorem_object.get_text()
+        self.assertGreater(500, len(typical_text))
+
+        greater_text = self.lorem_object.get_text(1000)
+        self.assertLess(500, len(greater_text))
+
+        small_text = self.lorem_object.get_text(100)
+        self.assertGreater(500, len(small_text))
